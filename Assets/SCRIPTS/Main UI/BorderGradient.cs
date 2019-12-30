bdf6683b18aaa555289fce824a14e70e01dab20c
FuzzyFish <ionesqueish@gmail.com>
2019-12-30 00:15:04 -0500

    Added UI gradient animations and borders
    Code, and art added for gradient animations

---
+++ Assets/SCRIPTS/Main UI/BorderGradient.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderGradient : MonoBehaviour
+{
+    public Material GradientFast;
+    public Material GradientSlow;
+
+    float xFast = 0.0f;
+    float xSlow = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        xFast += Time.deltaTime;
+        xSlow += Time.deltaTime * 0.35f;
+        GradientFast.SetTextureOffset("_MainTex", new Vector2(xFast, 0.0f));
+        GradientSlow.SetTextureOffset("_MainTex", new Vector2(xSlow, 0.0f));
+    }
+}
---
+++ Assets/SCRIPTS/Main UI/RenderTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTime : MonoBehaviour
+{
+    public Universal U;
+    public GUIStyle DrawRight;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(U.Scale_X(0.0f), U.Scale_Y(0.8f), U.Scale_X(1.0f), U.Scale_Y(0.2f)), "TIME TEST", DrawRight);
+    }
+}
